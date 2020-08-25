231be16c3f609497b0f42dc48fa7734b1fbf480f
Tahircan45 <tahircanakgun@gmail.com>
2020-08-25 11:29:12 +0300

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecureApiJWT.JWT;
+using SecureApiJWT.Models;
+
+namespace SecureApiJWT.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IJWTAuthManager _jWTAuthManager;
+        public UserController(IJWTAuthManager jWTAuthManager) 
+        {
+            _jWTAuthManager = jWTAuthManager;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [AllowAnonymous]
+        [HttpPost("AuthUser")]
+        public IActionResult AuthUser([FromBody] UserAuth userAuth)
+        {
+            var token=_jWTAuthManager.Auth(userAuth.UserName,userAuth.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,69 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using SecureApiJWT.JWT;
+
+namespace SecureApiJWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var Key = "this my test key";
+            services.AddControllers();
+            services.AddSingleton<IJWTAuthManager>(new JWTAuthManager(Key));
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(x =>
+            {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
