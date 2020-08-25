04da3a4a915e5220fe24858b4b903282e5bc7801
Tahircan45 <tahircanakgun@gmail.com>
2020-08-25 17:46:57 +0300

    Revert "Swagger Added"
    This reverts commit 8f88dbc46aa07510a0a3b9914f08565d4f27377f.

--- Startup.cs
+++ Startup.cs
@@ -22,8 +22,6 @@ namespace SecureApiJWT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSwaggerGen();
-
             var Key = "this my test key";
             services.AddControllers();
             services.AddSingleton<IJWTAuthManager>(new JWTAuthManager(Key));
@@ -56,13 +54,9 @@ namespace SecureApiJWT
             }
 
             app.UseHttpsRedirection();
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
+
             app.UseRouting();
-            
+
             app.UseAuthentication();
             app.UseAuthorization();
 
